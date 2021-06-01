9af8df01406a797e15050f00a1c9b89abb5f386a
Melle Meewis <MelleMeewis@MacBook-Pro-van-Pepijn.local>
2021-06-01 12:52:05 +0200

    minor

--- models/GAN.py
+++ models/GAN.py
@@ -449,7 +449,7 @@ class StyleGAN:
 
     def __return_vae_probability(self, cur_depth, cur_epoch, epochs):
         epochs = epochs[cur_depth]
-        start, end = self.vae_probs(cur_depth)
+        start, end = self.vae_probs[cur_depth]
         grow = (end - start) / total_epochs
         return start + grow * cur_epoch
 
