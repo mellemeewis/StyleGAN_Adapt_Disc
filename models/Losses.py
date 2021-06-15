76c1abaf27cca64674930d1689917e71f4f07289
Melle Meewis <MelleMeewis@MacBook-Pro-van-Pepijn.local>
2021-06-15 16:10:13 +0200

    minor

--- models/Losses.py
+++ models/Losses.py
@@ -270,7 +270,6 @@ class LogisticGAN(GANLoss):
         kl_loss = 0.5 * torch.mean(latents[:, :, l//2:].exp() - latents[:, :, l//2:] + latents[:, :, :l//2].pow(2) - 1, dim=1)
         latents = latents[:, :, :l//2] + Variable(torch.randn(b, w, l//2).to(latents.device)) * (latents[:,:, l//2:] * 0.5).exp()
 
-        print(latents.size())
         reconstrution = self.gen(latents, height, alpha)
 
         # reconstrution_distribution = torch.distributions.continuous_bernoulli.ContinuousBernoulli(reconstrution)
