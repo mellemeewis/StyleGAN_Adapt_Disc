5ad20a4a52c0586f45b494758119cbf2d4395b5a
Melle meewis <mellemeewi@icloud.com>
2021-06-18 15:07:22 +0200

    m

--- scripts/vae_gan_single_latent.sh
+++ scripts/vae_gan_single_latent.sh
@@ -28,7 +28,7 @@ cd /home/mms496/StyleVAE_Experiments/stylegan_adapt_disc
 
 
 
-python -u /home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch-adapt_disc/train.py --config '/home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch-adapt_disc/configs/vae+vae+gan_single_latent.yaml'
+python -u /home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch-adapt_disc/train.py --config '/home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch-adapt_disc/configs/vae_gan_single_latent.yaml'
 wait          # wait until programs are finished
 
 # echo $$
--- scripts/vae_halfgan_single_latent.sh
+++ scripts/vae_halfgan_single_latent.sh
@@ -28,7 +28,7 @@ cd /home/mms496/StyleVAE_Experiments/stylegan_adapt_disc
 
 
 
-python -u /home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch-adapt_disc/train.py --config '/home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch-adapt_disc/configs/vae+halfgan_single_latent.yaml'
+python -u /home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch-adapt_disc/train.py --config '/home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch-adapt_disc/configs/vae_halfgan_single_latent.yaml'
 wait          # wait until programs are finished
 
 # echo $$
