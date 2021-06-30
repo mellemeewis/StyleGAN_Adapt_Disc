9d7b6962a2b187038d926a718bda3c33c9f6a4d1
Melle meewis <mellemeewi@icloud.com>
2021-06-30 17:39:47 +0200

    m

--- models/Generator.py
+++ models/Generator.py
@@ -245,9 +245,7 @@ class Generator(nn.Module):
         """
 
         if not latent_are_in_extended_space:
-            b,l = latents_in.size()
             dlatents_in = self.g_mapping(latents_in)
-            dlatents_in = dlatents_in.view(b,-1,l)
         else:
             dlatents_in = latents_in
 
