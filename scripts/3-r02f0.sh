eaa927a5d90a722511873ac65984ab35b7eeec88
Melle meewis <mellemeewi@icloud.com>
2021-07-22 17:47:16 +0200

    m

--- scripts/3-r02f0.sh
+++ scripts/3-r02f0.sh
@@ -28,7 +28,7 @@ cd /home/mms496/StyleVAE_Experiments/stylegan_adapt_disc
 
 
 
-python -u /home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch-adapt_disc/train.py --config '/home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch-adapt_disc/configs/3-r25f25.yaml'
+python -u /home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch-adapt_disc/train.py --config '/home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch-adapt_disc/configs/3-r02f0.yaml'
 wait          # wait until programs are finished
 
 # echo $$
