776170f9b8ca6712277abfdf601b5dabda00f1fe
Melle meewis <mellemeewi@icloud.com>
2021-07-27 17:17:03 +0200

    m

--- scripts/mnist/bce-r1f0.sh
+++ scripts/mnist/bce-r1f0.sh
@@ -28,7 +28,7 @@ cd /home/mms496/StyleVAE_Experiments/stylegan_adapt_disc
 
 
 
-python -u /home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch-adapt_disc/train.py --config '/home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch-adapt_disc/configs/mnist/bce-r10f10.yaml'
+python -u /home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch-adapt_disc/train.py --config '/home/mms496/StyleVAE_Experiments/code/StyleGAN.pytorch-adapt_disc/configs/mnist/bce-r1f0.yaml'
 wait          # wait until programs are finished
 
 # echo $$
